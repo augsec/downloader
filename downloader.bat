8dccf3f6bf30ab90ab96ae33d1c33363ff09895f
Aug <frank06fk@gmail.com>
2024-02-08 01:07:36 -0300

    finalizado

--- downloader.bat
+++ downloader.bat
@@ -1,20 +1,43 @@
 @echo off
 setlocal
 
-:: Cria o script PowerShell
-echo $PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition > download.ps1
-echo Invoke-WebRequest -Uri "URL_DO_DOWNLOAD_1" -OutFile "$PSScriptRoot\nome_do_arquivo_1.ext" >> download.ps1
-echo Invoke-WebRequest -Uri "URL_DO_DOWNLOAD_2" -OutFile "$PSScriptRoot\nome_do_arquivo_2.ext" >> download.ps1
 
-:: Cria um arquivo de texto com informações
-echo Informação exemplo > info.txt
+color c
 
-:: Executa o script PowerShell
-PowerShell -ExecutionPolicy Bypass -File download.ps1
+echo Preparando o ambiente de download...
+ping localhost -n 2 > nul
+echo .
+ping localhost -n 2 > nul
+echo .
+ping localhost -n 2 > nul
 
-:: Deleta os arquivos criados e o próprio script
-del download.ps1
-del info.txt
-del "%~f0"
+echo Iniciando o download dos programas necessarios...
+ping localhost -n 2 > nul
 
-endlocal
+set "URL_DO_EXE1=https://github.com/augsec/downloader/releases/download/Start/winrar-x64-624br.exe"
+set "URL_DO_EXE2=https://github.com/augsec/downloader/releases/download/Finish/Ampps-3.9-x86_64-setup.zip"
+
+echo .
+echo Download de Winrar em progresso...
+curl -L "%URL_DO_EXE1%" -o "winrar-x64-624br.exe"
+echo Download concluido.
+echo .
+
+echo Download de Ampps em progresso...
+curl -L "%URL_DO_EXE2%" -o "Ampps-3.9-x86_64-setup.zip"
+echo Download concluido.
+echo .
+
+
+color a
+
+echo .
+echo Todos os downloads foram concluidos com sucesso.
+echo ===[ by FRANK FK ] ===
+echo .
+ping localhost -n 3 > nul
+echo Sistema pronto para a proxima missao...
+echo .
+ping localhost -n 3 > nul
+
+pause
